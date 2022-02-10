e7165d29b2d9cbcd875698d6327ede3c84a499f9
sumita <sumita.k@canopas.com>
2022-02-10 16:45:59 +0530

    Change port

--- main.go
+++ main.go
@@ -2,43 +2,40 @@ package main
 
 import (
 	"contact"
-	"db"
 	"embed"
-	"jobs"
 	"log"
 	"os"
 
 	"github.com/apex/gateway"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/jmoiron/sqlx"
 )
 
 //go:embed templates/email-template.html
 var tmplFS embed.FS
 
 func main() {
-	sqlDb := db.NewSql()
-	defer sqlDb.Close()
+	// sqlDb := db.NewSql()
+	// defer sqlDb.Close()
 
-	r := setupRouter(sqlDb)
+	r := setupRouter()
 
 	_ = r.Run(":8080")
 }
 
-func setupRouter(sqlDb *sqlx.DB) *gin.Engine {
+func setupRouter() *gin.Engine {
 	router := gin.Default()
 
 	router.Use(cors.New(corsConfig()))
 
 	contactRepo := contact.New(tmplFS)
-	jobsRepo := jobs.New(sqlDb)
+	// jobsRepo := jobs.New(sqlDb)
 
 	router.POST("/api/send-contact-mail", contactRepo.SendContactMail)
 
-	router.GET("/api/careers", jobsRepo.Careers)
+	// router.GET("/api/careers", jobsRepo.Careers)
 
-	router.GET("/api/career/:id", jobsRepo.CareerById)
+	// router.GET("/api/career/:id", jobsRepo.CareerById)
 
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
