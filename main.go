ae0491fb2bcc67fb62c0ade097cf25fd4830bac3
jimmy0251 <jimmy0251@gmail.com>
2022-02-11 13:30:43 +0530

    Fix lambda SQL access

--- main.go
+++ main.go
@@ -2,42 +2,45 @@ package main
 
 import (
 	"contact"
+	"db"
 	"embed"
+	"jobs"
 	"log"
 	"os"
 
 	"github.com/apex/gateway"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
 )
 
 //go:embed templates/email-template.html
 var tmplFS embed.FS
 
 func main() {
-	// sqlDb := db.NewSql()
-	// defer sqlDb.Close()
+	sqlDb := db.NewSql()
+	defer sqlDb.Close()
 
-	r := setupRouter()
+	r := setupRouter(sqlDb)
 
 	_ = r.Run(":8080")
 }
 
-func setupRouter() *gin.Engine {
+func setupRouter(sqlDb *sqlx.DB) *gin.Engine {
 	router := gin.Default()
 
 	router.Use(cors.New(corsConfig()))
 
 	contactRepo := contact.New(tmplFS)
-	// jobsRepo := jobs.New(sqlDb)
+	jobsRepo := jobs.New(sqlDb)
 
 	router.POST("/api/send-contact-mail", contactRepo.SendContactMail)
 
-	// router.GET("/api/careers", jobsRepo.Careers)
+	router.GET("/api/careers", jobsRepo.Careers)
 
-	// router.GET("/api/career/:id", jobsRepo.CareerById)
+	router.GET("/api/careers/:id", jobsRepo.CareerById)
 
-	router.GET("/ping", func(c *gin.Context) {
+	router.GET("/api/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Pong",
 		})
