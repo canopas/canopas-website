612c451e297a7564581022909cf026cae44c72ef
sumita <sumita.k@canopas.com>
2022-03-07 17:19:24 +0530

    Update metadata for home page

--- vue-frontend/src/config.js
+++ vue-frontend/src/config.js
@@ -11,9 +11,9 @@ export default Object.freeze({
   CALENDLY_IFRAME_URL: "https://calendly.com/darpanvithani/15min",
   SEO_META_DATA: {
     title:
-      "Canopas - A software development company in surat helping businesses bring their idea to life!",
+      "Canopas - Fastest growing IT company in Surat, India. A growing software development company.",
     description:
-      "Canopas is a software company helping people like you(entrepreneurs, startups, small companies) bring your IDEA to LIFE by improving or developing digital products.",
+      "At Canopas, We develop amazing products to help entrepreneurs, startups, and businesses bring their ideas to life.",
     type: "Website",
     url: "https://dev-stack.canopas.com",
   },
