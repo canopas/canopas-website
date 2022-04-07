57eeb073d58bdb6a2db65f48492dc0dc9b4e4e3b
cp-shruti-s <shruti.s@canopas.com>
2022-04-07 10:53:17 +0530

    Implement portfolio section

--- vue-frontend/src/components/NewHomeScreen.vue
+++ vue-frontend/src/components/NewHomeScreen.vue
@@ -1,17 +1,21 @@
 <template>
   <div>
     <ScreenHeader />
+    <Portfolio />
   </div>
 </template>
 
 <script>
 import ScreenHeader from "./partials/ScreenHeader.vue";
+import Portfolio from "./home-new/PortfolioSection.vue";
+
 export default {
   data() {
     return {};
   },
   components: {
     ScreenHeader,
+    Portfolio,
   },
 };
 </script>
---
+++ vue-frontend/src/components/home-new/PortfolioSection.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="container-fluid mt-5">
+    <div class="text-center v2-header-text">Portfolio</div>
+    <div class="portfolio-section mt-5">
+      <div
+        v-for="portfolio in portfolios"
+        :key="portfolio"
+        class="portfolio-details mt-4"
+      >
+        <img :src="portfolio.image" class="portfolio-image" />
+        <div class="description">
+          <div class="v2-title-text">{{ portfolio.title }}</div>
+          <div class="v2-normal-text mt-3">
+            {{ portfolio.detail }}
+          </div>
+          <div class="mt-4">
+            <router-link class="v2-button v2-normal-2-text" to="">
+              <span>VIEW</span>
+              <font-awesome-icon
+                class="arrow fa"
+                icon="arrow-right"
+                id="leftArrow"
+              />
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="module">
+import luxeradio from "@/assets/images/portfolio/luxeradio.png";
+import noLonely from "@/assets/images/portfolio/noLonely.png";
+import smilePlue from "@/assets/images/portfolio/smilePlus.png";
+import togness from "@/assets/images/portfolio/togness.png";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+export default {
+  data() {
+    return {
+      luxeradio: luxeradio,
+      noLonely: noLonely,
+      portfolios: [
+        {
+          image: luxeradio,
+          title: "Luxeradio",
+          detail:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.",
+          link: "",
+        },
+        {
+          image: smilePlue,
+          title: "Smile+",
+          detail:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.",
+          link: "",
+        },
+        {
+          image: noLonely,
+          title: "NoLonely",
+          detail:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.",
+          link: "",
+        },
+        {
+          image: togness,
+          title: "Togness",
+          detail:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.",
+          link: "",
+        },
+      ],
+    };
+  },
+  components: {
+    FontAwesomeIcon,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.container-fluid {
+  padding: 0;
+}
+
+.portfolio-image {
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+}
+
+.portfolio-details {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.description {
+  padding: 5% 20%;
+}
+
+.arrow {
+  color: #3d3d3d;
+}
+
+@include media-breakpoint-up(md) {
+  .portfolio-details:nth-child(even) {
+    flex-direction: row-reverse;
+  }
+
+  .portfolio-details:nth-child(odd) {
+    flex-direction: row;
+  }
+
+  .description {
+    width: 35%;
+    margin: auto;
+    padding: 0;
+  }
+
+  .portfolio-image {
+    height: 50%;
+    width: 50%;
+  }
+}
+
+@include media-breakpoint-up(lg) {
+  .portfolio-image {
+    height: 40%;
+    width: 40%;
+  }
+}
+</style>
