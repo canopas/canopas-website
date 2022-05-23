1cdba55ddf0198a3077968d9e1a9546a34ea8cc5
Priyanka Gour <priyanka.g@canopas.com>
2022-05-23 14:43:09 +0530

    Add letter spacing for IOS

--- vue-frontend/src/components/home-new/LandingSection.vue
+++ vue-frontend/src/components/home-new/LandingSection.vue
@@ -380,4 +380,9 @@ export default {
     line-height: 5.875rem;
   }
 }
+@media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2) {
+  .v2-header-3-text {
+    letter-spacing: 0.5px;
+  }
+}
 </style>
