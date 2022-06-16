793cd9cab9e0d8cbce496c0934b37c36a0ef1505
Shruti Sonani <shruti.s@canopas.com>
2022-06-16 14:34:25 +0530

    Redesign contact page

--- vue-frontend/src/components/contact/ContactLanding.vue
+++ vue-frontend/src/components/contact/ContactLanding.vue
@@ -1,51 +1,92 @@
 <template>
-  <div class="container">
-    <div class="contact-title">Hey smarty,</div>
-    <div class="normal-text contact-description">
-      <p>
-        Thanks for reaching out to us. We’re happy to help you achieve your
+  <div class="background">
+    <div class="container tw-py-32">
+      <div class="v2-header-3-text v2-canopas-gradient-text">Hey Smarty,</div>
+      <div class="v2-title-2-text tw-pt-20">
+        Thanks for reaching out to us. We're happy to help you achieve your
         goals and dreams.
-      </p>
-      <p>
-        As you’re landed here, I guess you’re looking for help with your idea or
-        existing product and you’re wondering how our team can help you.
-      </p>
-      <p>
-        We will guide you through the process so you don’t have to worry about
-        it. Before that, you gotta fill up the form so we can know more about
-        you before we approach you.
-      </p>
+      </div>
+      <div class="v2-title-2-text tw-pt-12">
+        This form requires
+        <span class="canopas-gradient-text">less then 60 seconds</span>
+        to fill up. Here's what you will get by filling out this form.
+      </div>
+      <div>
+        <ul class="chat-list text-left mt-4">
+          <li>
+            <font-awesome-icon
+              class="tw-h-6 tw-w-4 tw-text-pink-300 tw-mr-6 tw-mt-2 lg:tw-mt-3"
+              icon="chevron-right"
+              aria-hidden="true"
+            />
+            <div class="tw-w-11/12 lg:tw-w-10/12 v2-normal-text">
+              Free consultation for 30 Minutes.
+            </div>
+          </li>
+          <li>
+            <font-awesome-icon
+              class="tw-h-6 tw-w-4 tw-text-pink-300 tw-mr-6 tw-mt-2 lg:tw-mt-3"
+              icon="chevron-right"
+              aria-hidden="true"
+            />
+            <div class="tw-w-11/12 lg:tw-w-10/12 v2-normal-text">
+              100% MONEY BACK GUARANTEE for the first TWO WEEKS of work if
+              you're not satisfied. No questions asked.
+            </div>
+          </li>
+          <li>
+            <font-awesome-icon
+              class="tw-h-6 tw-w-4 tw-text-pink-300 tw-mr-6 tw-mt-2 lg:tw-mt-3"
+              icon="chevron-right"
+              aria-hidden="true"
+            />
+            <div class="tw-w-11/12 lg:tw-w-10/12 v2-normal-text">
+              An opportunity to grow your business exponentially.
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
 export default {
-  components: {},
+  data() {
+    return {};
+  },
+  components: {
+    FontAwesomeIcon,
+  },
 };
 </script>
 
-<style lang="scss" scoped>
-.container {
-  margin: 48px auto;
-  padding: 0 5%;
+<style lang="postcss" scoped>
+.background {
+  background: linear-gradient(
+    270.11deg,
+    rgba(249, 131, 134, 0.05) -24.42%,
+    rgba(249, 131, 134, 0.05) 101.76%
+  );
+  box-shadow: 0px 4px 2px 1px rgba(0, 0, 0, 5%);
+  z-index: -1;
 }
 
-.contact-title {
-  margin-bottom: 32px;
-  font-size: 2.25rem;
-  font-weight: bold;
-  letter-spacing: 1px;
-  background: linear-gradient(89.88deg, #f2709c -1.25%, #ff9472 28.15%);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
+.chat-list {
+  list-style-type: none;
+  margin: auto;
+  padding: 0;
 }
 
-.contact-description {
-  line-height: 1.5rem;
+.chat-list > li {
+  padding: 10px 0;
+  display: flex;
+  align-items: flex-start;
 }
 
-.contact-description > p {
-  margin-bottom: 24px;
+.v2-title-2-text {
+  @apply tw-leading-9 lg:tw-leading-11;
 }
 </style>
