6f455c6a71fbfae78ded6a7fb80b3a45eee42f7b
jagruti <jagruti.a@canopas.com>
2023-02-17 12:34:15 +0530

    Redesign Portfolio page

---
+++ vue-frontend/src/components/home-new/NewPortfolioCTASection.vue
@@ -0,0 +1,77 @@
+<template>
+  <div
+    class="tw-mt-[20px] lg:tw-mt-[50px] xl:tw-mt-[100px] tw-bg-[#F8F8F8] tw-font-inter-regular"
+  >
+    <div
+      class="tw-container tw-flex tw-flex-col lg:tw-flex-row xl:tw-max-w-[1878px] lg:tw-h-[390px] xl:tw-h-full"
+    >
+      <div
+        class="tw-order-2 lg:tw-order-first lg:tw-w-[50%] xl:tw-w-[40%] 2xl:tw-pl-[30px]"
+      >
+        <img
+          :src="CTAImage[0]"
+          :srcset="`${CTAImage[0]} 400w, ${CTAImage[1]} 800w, ${CTAImage[2]} 1600w, ${CTAImage[3]} 2400w`"
+          alt="portfolio-cta-image"
+          class="-tw-mb-[1rem] sm:-tw-mb-[1.5rem] md:-tw-mb-[2rem] xl:-tw-mb-[1.25rem] lg:-tw-mt-[3.5rem]"
+          loading="lazy"
+        />
+      </div>
+
+      <div class="tw-flex lg:tw-w-[50%]">
+        <div
+          class="tw-m-auto lg:tw-ml-auto xl:tw-m-auto tw-w-[80%] sm:tw-w-[60%] md:tw-w-[50%] lg:tw-w-[90%] 2xl:tw-w-9/12 tw-py-[40px] lg:tw-py-0"
+        >
+          <h1
+            class="tw-text-black-core/[0.87] tw-text-[1.5rem] tw-leading-[1.813rem] md:tw-text-[2.188rem] md:tw-leading-[2.625rem] xl:tw-text-[3.125rem] xl:tw-leading-[3.813rem] tw-font-inter-bold"
+          >
+            Whether you need...
+          </h1>
+          <ul
+            class="tw-pt-6 tw-text-[1rem] tw-text-black-core/[0.6] tw-leading-[1.25rem] md:tw-text-[1.188rem] md:tw-leading-[1.5rem] xl:tw-text-[1.375rem] xl:tw-leading-[1.8125rem] tw-font-inter-semibold"
+          >
+            <li v-for="reason in reasons" :key="reason" class="tw-flex">
+              <span class="tw-pr-1">*</span>
+              <div class="sm:tw-w-[90%]">{{ reason }}</div>
+            </li>
+          </ul>
+          <div
+            class="xl:tw-w-[85%] tw-pt-6 tw-pb-8 tw-text-black-core/[0.6] tw-font-inter-regular tw-text-[0.875rem] tw-leading-[1.125rem] md:tw-text-[1.125rem] md:tw-leading-[1.375rem] xl:tw-text-[1.375rem] xl:tw-leading-[1.813rem]"
+          >
+            Bring us your toughest challenge and we'll show you the path to a
+            sleek solution.
+          </div>
+          <router-link
+            class="tw-flex tw-items-center tw-mx-auto lg:tw-mx-0 tw-w-max tw-rounded-full tw-p-3 tw-text-center tw-font-normal tw-text-[1rem] tw-leading-[1.1875rem] md:tw-text-[1.09375rem] md:tw-leading-[1.3125rem] xl:tw-text-[1.1875rem] xl:tw-leading-[1.4375rem] tw-font-inter-medium !tw-tracking-[0] gradient-btn hover:tw-text-white active:tw-scale-[0.98]"
+            to="/contact"
+            @click.native="mixpanel.track('tap_home_portfolio_cta')"
+          >
+            <span class="tw-text-white tw-font-inter-semibold"
+              >Talk to our experts</span
+            >
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="module">
+import cta400 from "@/assets/images/portfolio/new-portfolio/new-cta-400w.webp";
+import cta800 from "@/assets/images/portfolio/new-portfolio/new-cta-800w.webp";
+import cta1600 from "@/assets/images/portfolio/new-portfolio/new-cta-1600w.webp";
+import cta2400 from "@/assets/images/portfolio/new-portfolio/new-cta-2400w.webp";
+
+export default {
+  data() {
+    return {
+      CTAImage: [cta400, cta800, cta1600, cta2400],
+      reasons: [
+        "High-performing mobile apps",
+        "Bulletproof cloud solutions",
+        "Custom solutions for your business.",
+      ],
+    };
+  },
+  inject: ["mixpanel"],
+};
+</script>
--- vue-frontend/src/pages/portfolio/index.vue
+++ vue-frontend/src/pages/portfolio/index.vue
@@ -6,19 +6,104 @@
       </template>
     </metainfo>
     <Header />
-    <Portfolio />
-    <CTA />
+    <div
+      class="tw-text-center v2-header-text tw-font-futura-bold tw-mt-8 lg:tw-mt-36"
+    >
+      Portfolio
+    </div>
+    <div
+      class="tw-container tw-flex tw-justify-around tw-m-auto tw-mt-12 lg:tw-mt-44 tw-w-[100%] sm:tw-w-[95%] 2xl:tw-w-[45%] tw-rounded-[30px] tw-bg-white tw-py-[5px] tw-drop-shadow-[0_2px_10px_rgba(61,61,61,0.1)]"
+    >
+      <a
+        v-for="(navbar, index) in navbars"
+        :key="index"
+        class="tw-flex-[16.66%] tw-m-0 tw-w-full tw-rounded-[25px] tw-px-[5px] tw-py-2 md:tw-py-[13px] tw-text-center tw-tracking-[1px] tw-text-[0.75rem] tw-leading-[0.938rem] md:tw-text-[1rem] md:tw-leading-[1.1875rem] lg:tw-text-[1.188rem] lg:tw-leading-[1.438rem] tw-font-inter-semibold active:tw-scale-[0.98] portfolio-nav"
+        :class="[
+          currentRoutePath == navbar.url
+            ? 'gradient-btn hover:tw-text-[#f2709c]'
+            : 'tw-border-[1px] tw-border-solid tw-border-transparent hover:tw-from-[#ff835b] hover:tw-to-[#f2709c] hover:tw-bg-gradient-[270.11deg] tw-text-black-core/[0.6] hover:tw-text-white',
+
+          index == navbars.length - 1 ? '' : 'sm:tw-mr-2.5',
+        ]"
+        :href="navbar.url"
+        @click.native="mixpanel.track(navbar.event)"
+        :target="navbar.target ? '_blank' : '_self'"
+      >
+        {{ navbar.name }}
+      </a>
+    </div>
+
+    <div
+      class="tw-container tw-py-12 md:tw-py-20 lg:tw-py-28 tw-text-[1.375rem] tw-leading-[1.625rem] md:tw-text-[1.9375rem] md:tw-leading-[1.812rem] lg:tw-text-[2.5rem] lg:tw-leading-[2rem] tw-text-black-core/[0.87] tw-text-center tw-font-inter-medium"
+    >
+      <span
+        class="tw-relative after:tw-absolute after:-tw-bottom-[2px] after:tw-right-0 after:tw-content-[''] after:tw-w-[116px] md:after:tw-w-[152px] lg:after:tw-w-[205px] after:tw-h-[10px] md:after:tw-h-[14px] after:tw-bg-[#FFF3F7] after:-tw-z-[1] v2-canopas-gradient-text"
+        >1M+ users</span
+      >
+      use our apps every day
+    </div>
+    <PortfolioSection ref="newPortfolio" />
+    <NewPortfolioCTASection />
+    <UserReview class="sm:tw-top-[40px]" />
+    <CTASection ref="cta" />
+    <NewFooter />
   </div>
 </template>
 
 <script>
 import Header from "@/components/partials/NewHeader.vue";
-import Portfolio from "@/components/home/PortfolioSection.vue";
 import CTA from "@/components/home/CTASection.vue";
 import { useMeta } from "vue-meta";
 import config from "@/config.js";
+import { elementInViewPort } from "@/utils.js";
+import PortfolioSection from "@/components/home-new/PortfolioSection.vue";
+import NewPortfolioCTASection from "@/components/home-new/NewPortfolioCTASection.vue";
+import UserReview from "@/components/home/UserReview.vue";
+import CTASection from "@/components/home-new/CTASection.vue";
+import NewFooter from "@/components/partials/NewFooter.vue";
 
 export default {
+  data() {
+    return {
+      currentRoutePath: this.$router.currentRoute._value.path,
+      event: "",
+      navbars: [
+        {
+          name: "All",
+          url: "/portfolio",
+          target: false,
+        },
+        {
+          name: "Justly",
+          url: "/portfolio/justly",
+          target: false,
+          event: "tap_home_justly_portfolio",
+        },
+        {
+          name: "Luxeradio",
+          url: "/portfolio/luxeradio",
+          target: false,
+          event: "tap_home_luxe_radio_portfolio",
+        },
+        {
+          name: "Togness",
+          url: "/portfolio/togness",
+          target: false,
+          event: "tap_home_togness_portfolio",
+        },
+        {
+          name: "Smile+",
+          url: config.SMILEPLUS_URL,
+          target: true,
+          event: "tap_home_smile_portfolio",
+        },
+      ],
+      events: {
+        newPortfolio: "view_portfolio_section",
+        newPortfolioCTA: "view_home_portfolio_cta",
+      },
+    };
+  },
   setup() {
     var seoData = config.PORTFOLIO_SEO_META_DATA;
     useMeta({
@@ -33,8 +118,11 @@ export default {
   },
   components: {
     Header,
-    Portfolio,
-    CTA,
+    PortfolioSection,
+    NewPortfolioCTASection,
+    UserReview,
+    CTASection,
+    NewFooter,
   },
   inject: ["mixpanel"],
   mounted() {
