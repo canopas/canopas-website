ad27f41cde5add8bc5216603846f6b2f672f455a
Priyanka Gour <priyanka.g@canopas.com>
2023-06-13 14:51:14 +0530

    Add mobile view

--- vue-frontend/src/components/contribution/UnitTest.vue
+++ vue-frontend/src/components/contribution/UnitTest.vue
@@ -3,7 +3,7 @@
     class="tw-container tw-my-[1rem] lg:tw-mt-[5rem] tw-mx-auto tw-text-center"
   >
     <span
-      class="tw-text-center tw-font-inter-bold tw-text-black-core/[0.8] tw-text-[3.4375rem] tw-leading-[5.15625rem]"
+      class="tw-text-center tw-font-inter-bold tw-text-black-core/[0.8] tw-text-[1.875rem] lg:tw-text-[3.4375rem] tw-leading-[2.4375rem] lg:tw-leading-[5.15625rem]"
       >WHY and How to Write Unit Tests?</span
     >
     <div class="swiper-content tw-mt-16">
@@ -16,21 +16,27 @@
         }"
         :loop="true"
         :loopedSlides="50"
-        :spaceBetween="20"
+        :spaceBetween="10"
         :pagination="pagination"
-        @swiper="onSwiper"
         class="swiper-container"
       >
         <swiper-slide
-          v-for="image in images"
-          :key="image"
+          v-for="test in tests"
+          :key="test"
           class="tw-cursor-pointer"
         >
-          <img
-            :src="image"
-            class="tw-w-fit tw-h-fit tw-object-contain"
-            alt="UnitTestImage"
-          />
+          <div
+            class="tw-w-[350px] tw-h-[286px] lg:tw-hidden tw-rounded-b-lg tw-drop-shadow-md tw-bg-white"
+          >
+            <img
+              :src="test.image"
+              class="tw-w-fit tw-h-fit tw-object-contain tw-border-b-[0.5px] tw-border-black-core/[0.87] tw-rounded-t-lg"
+              alt="UnitTestImage"
+            />
+            <span class="tw-font-inter-medium tw-text-black-core/[0.87]">{{
+              test.title
+            }}</span>
+          </div>
         </swiper-slide>
       </swiper>
     </div>
@@ -45,12 +51,34 @@ import unittest2 from "@/assets/images/ContributionPage/unittest/unit_test_2.web
 import unittest3 from "@/assets/images/ContributionPage/unittest/unit_test_3.webp";
 import unittest4 from "@/assets/images/ContributionPage/unittest/unit_test_4.webp";
 SwiperCore.use([Pagination, Autoplay]);
-import Config from "@/config.js";
+
 export default {
   data() {
     return {
       swiper: null,
-      images: [unittest1, unittest2, unittest3, unittest4],
+      tests: [
+        {
+          image: unittest1,
+          title:
+            "The Ultimate Guide to iOS unit testing with best practices — Part 1",
+        },
+        {
+          image: unittest2,
+          title:
+            "The Ultimate Guide to iOS unit testing with best practices — Part 1 ",
+        },
+
+        {
+          image: unittest3,
+          title:
+            "The Ultimate Guide to iOS unit testing with best practices — Part 1 ",
+        },
+        {
+          image: unittest4,
+          title:
+            "The Ultimate Guide to iOS unit testing with best practices — Part 1 ",
+        },
+      ],
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
