8275a8e126ef8fc3c4db9e271ca730747aa3700a
Ashish <ashish.k@canopas.com>
2023-10-23 11:41:18 +0530

    Enhance:Design landing section of flutter development page

---
+++ vue-frontend/src/components/flutter-app-development/LandingSection.vue
@@ -0,0 +1,72 @@
+<template>
+  <section class="tw-mt-6 lg:tw-mt-[6.75rem]">
+    <div
+      class="tw-container lg:tw-text-left tw-text-center lg:tw-flex xl:tw-my-8 tw-pb-16"
+    >
+      <div
+        class="tw-flex tw-flex-col tw-gap-4 lg:tw-gap-6 lg:tw-flex-1 lg:tw-items-start"
+      >
+        <h1 class="header-1">Flutter App Development Company</h1>
+        <p class="sub-h1-regular tw-text-black-core/[0.89]">
+          We harness the power of Flutter to create visually stunning, fast, and
+          highly responsive applications that effortlessly work on both iOS and
+          Android platforms. Our team turns your unique business requirements
+          into reality, giving your brand the competitive edge it needs in this
+          dynamic mobile app market.
+        </p>
+        <div class="tw-hidden lg:tw-block">
+          <router-link
+            to="/contact"
+            @click.native="mixpanel.track('tap_flutter_development_cta')"
+          >
+            <button class="gradient-btn primary-btn tw-ml-0">
+              <span class="sub-h4-semibold tw-border-none"
+                >Get Free Consultation</span
+              >
+            </button>
+          </router-link>
+        </div>
+      </div>
+      <div
+        class="tw-mx-20 tw-mt-8 lg:tw-mt-0 lg:tw-mx-0 tw-flex lg:tw-flex-wrap tw-flex-col md:tw-gap-[1rem] lg:tw-flex-1"
+      >
+        <img
+          :src="landing400"
+          :srcset="`${landing400} 400w, ${landing800} 800w, ${landing1200} 1200w, ${landing2400} 2400w`"
+          class="tw-h-full tw-w-full tw-object-contain"
+          alt="flutter-landing"
+        />
+        <div class="lg:tw-hidden">
+          <router-link
+            to="/contact"
+            @click.native="mixpanel.track('tap_flutter_development_cta')"
+          >
+            <button class="gradient-btn primary-btn">
+              <span class="sub-h3-semibold tw-border-none"
+                >Get Free Consultation</span
+              >
+            </button>
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import landing400 from "@/assets/images/flutter-app-development/landing/landing400w.webp";
+import landing800 from "@/assets/images/flutter-app-development/landing/landing800w.webp";
+import landing1200 from "@/assets/images/flutter-app-development/landing/landing1200w.webp";
+import landing2400 from "@/assets/images/flutter-app-development/landing/landing2400w.webp";
+export default {
+  data() {
+    return {
+      landing400,
+      landing800,
+      landing1200,
+      landing2400,
+    };
+  },
+  inject: ["mixpanel"],
+};
+</script>
